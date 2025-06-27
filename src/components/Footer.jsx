ecfb0f529c2825ac6d39f756200d5d0d3528cf19
Eshan <2016mehrab@gmail.com>
2025-06-27 22:29:51 +0600

    style(Footer): update footer copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-4 bg-muted text-center text-muted-foreground">
-      <p>&copy; 2025 My Blog. All rights reserved.</p>
+
+    <footer className="p-4 bg-muted text-center text-muted-background">
+      <p>&copy; 2025 <span className="font-semibold">Minimal-Blog</span> by <span className="font-bold">Mehrab Hasan Eshan</span>. All rights reserved.</p>
     </footer>
+
   );
 };
 
